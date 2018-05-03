8653629d1b30b416fc63d4f8add35a5e04ad1c47
Timotius Margo <timargo@microsoft.com>
2018-05-02 19:49:19 -0700

    Update nuspec and assembly info to generate a preview NuGet package Add SignConfig.xml to enable proper binary signing in VSTS build

--- appium-dotnet-driver/Properties/AssemblyInfo.cs
+++ appium-dotnet-driver/Properties/AssemblyInfo.cs
@@ -17,12 +17,12 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("appium-dotnet-driver")]
-[assembly: AssemblyDescription("Appium Dotnet Driver")]
+[assembly: AssemblyTitle("winappdriver-preview-appium-dotnet-driver")]
+[assembly: AssemblyDescription("Microsoft WinAppDriver Appium Dotnet Driver Preview")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Appium Contributors")]
-[assembly: AssemblyProduct("Appium-Dotnet-Driver")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("WinAppDriver-Preview-Appium-Dotnet-Driver")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,7 +30,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyInformationalVersion("4.0.0.1-beta")]
+[assembly: AssemblyInformationalVersion("0.0.0.1")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
